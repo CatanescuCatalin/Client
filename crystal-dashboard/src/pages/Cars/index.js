51db49b11578a4e35a1394504a27476a96ae04f1
DicuRazvanGabiel <dicu.razvan.gabriel@gmail.com>
2019-06-13 17:50:11 +0300

    init

---
+++ crystal-dashboard/src/pages/Cars/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Cars extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Cars
+                </h1>
+            </div>
+        )
+    }
+}
